5bf80eb55f1e79cce10e32fabf72a07de1dc4bec
dami rebergen <damirebergen@gmail.com>
2022-08-03 13:07:19 +0200

    add push statement to docker build

--- create_doker.sh
+++ create_doker.sh
@@ -1,3 +1,6 @@
 conda activate cyclomics-nf-cyclomicseq
 conda env export > environment.yml
-docker build . -t damicyclomics/cyclomicseq:0.0.3
+docker build . -t damicyclomics/cyclomicseq:0.4.7
+docker tag damicyclomics/cyclomicseq:0.4.7 damicyclomics/cyclomicseq:latest
+docker push damicyclomics/cyclomicseq:0.4.7
+docker push damicyclomics/cyclomicseq:latest
