a2f645d8b809098cc92abacc6330ae763a988777
groodri <22616857+groodri@users.noreply.github.com>
2025-11-27 12:49:48 +0100

    Remove broken subtree

--- Cycas/create_docker.sh
+++
@@ -1,9 +0,0 @@
-
-CONTAINER_VERSION="0.6.0"
-CONTAINER_NAME="cycas"
-
-docker build . -t cyclomics/$CONTAINER_NAME:$CONTAINER_VERSION
-docker tag cyclomics/$CONTAINER_NAME:$CONTAINER_VERSION cyclomics/$CONTAINER_NAME:latest
-docker push cyclomics/$CONTAINER_NAME:$CONTAINER_VERSION
-docker push cyclomics/$CONTAINER_NAME:latest
-
--- Cycas/cycas/src/calibrated_consensus/algorithms/__init__.py
+++
@@ -1,1 +0,0 @@
-from .fraction import FractionCalibratedConsensus as FractionCalibratedConsensus
