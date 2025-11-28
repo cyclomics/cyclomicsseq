cafc4841d0cc5ff3b8f1b523e3b65fa43c618d22
groodri <rdrigmns@hotmail.com>
2025-11-28 14:41:51 +0100

    fix: make container amd64 compatible

--- create_docker.sh
+++ create_docker.sh
@@ -1,4 +1,6 @@
-docker build . -t cyclomics/cyclomicsseq:2.0.0-rc1
+docker buildx create --use
+docker buildx build --platform linux/amd64,linux/arm64 \
+  -t cyclomics/cyclomicsseq:2.0.0-rc1 .
 docker tag cyclomics/cyclomicsseq:2.0.0-rc1 cyclomics/cyclomicsseq:latest
 docker push cyclomics/cyclomicsseq:2.0.0-rc1
 docker push cyclomics/cyclomicsseq:latest
